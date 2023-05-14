2f3063fc3671804635272d145b06efffe4c122f4
Suryana <nanasuryana335@gmail.com>
2023-05-14 10:50:17 +0700

    feat: Initial new-commit
    These are a new commit of the e-shop project
    this project previously has been added with
    angular products and orders module. Inside
    of products module there are product-list
    component.

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ProductsModule } from './products/products.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ProductsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
