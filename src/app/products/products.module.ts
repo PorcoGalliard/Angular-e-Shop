1ed3cfe22fd9193f62fefc283b539caba366e7bd
Suryana <nanasuryana335@gmail.com>
2023-05-14 11:55:47 +0700

    feat: expose ProductList component to app.component.html

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
 
 @NgModule({
   declarations: [
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductComponent
+  ],
+  exports: [
+    ProductListComponent,
   ]
 })
 export class ProductsModule { }
